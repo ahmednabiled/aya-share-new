8f58e57c805c57a2bb24de58e7ac88278d6d2ded
Ahmed Mohamed <ahmedmohamed2000771@gmail.com>
2025-12-03 14:08:56 +0200

    added logout route

--- backend/src/modules/Auth/Auth.routes.ts
+++ backend/src/modules/Auth/Auth.routes.ts
@@ -1,6 +1,7 @@
 import express, { type Request, type Response} from "express";
 import passport from "../../config/passport.js";
 import { handleCallBack } from "./Auth.controller.js";
+import { isAuth } from "./Auth.service.js";
 
 const router = express.Router();
 
@@ -12,6 +13,13 @@ router.get("/google/callback", passport.authenticate("google", {
     failureRedirect: "/login", //to do
     session: false
 }), handleCallBack);
+
+router.post("/logout", isAuth, (req: Request, res: Response) => {
+    res.clearCookie("refreshtoken");
+    res.status(200).json({
+        status: "Success"
+    })
+})
 
 
 export default router;
